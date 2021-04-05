bd7df71c6711f46ff07df7bb6c9ea8b55e901d66
Krinut <krinut7@gmail.com>
2021-04-05 20:37:24 +0900

    first commit

--- random-practise/chapter15/random_walk.py
+++ random-practise/chapter15/random_walk.py
@@ -3,36 +3,38 @@
 from random import choice	#this will import the choice module to be used
 
 class RandomWalk:
-	"""a class to generate random walks"""
+    """a class to generate random walks"""
 
-	def __init__(self, num_points=5000):
-		"""initialising the attributes"""
-		self.num_points = num_points
-		self.x_values = [0]
-		self.y_values = [0]
+    def __init__(self, num_points=5000):
+        """initialising the attributes"""
+        self.num_points = num_points
+        self.x_values = [0]
+        self.y_values = [0]
+        self.locations = [(0,0)]
+	
+    def get_step(self):
+        """calculating the distance and direction of step"""
+        direction = choice([-1, 1])
+        distance = choice([0, 1, 2, 3, 4])
+        step = direction * distance
+        return step
 
-	def get_step(self):
-		"""calculating the distance and direction of step"""
-		direction = choice([-1, 1])
-		distance = choice([0, 1, 2, 3, 4])
-		step = direction * distance
-		return step
+    def fill_walk(self):
+        """appending the step to the already existing files"""
+        #keep walking till the points are reached
+        while len(self.x_values) < self.num_points:
+            x_step = self.get_step()
+            y_step = self.get_step()
 
-	def fill_walk(self):
-		"""appending the step to the already existing files"""
-		#keep walking till the points are reached
-		while len(self.x_values) < self.num_points:
-			x_step = self.get_step()
-			y_step = self.get_step()
+            #if the step size is zero just ignore this and move on to the next iteration
+            if x_step == 0 and y_step == 0:
+                continue
 
-			#if the step size is zero just ignore this and move on to the next iteration
-			if x_step == 0 and y_step == 0:
-				continue
+            #calculate the new position
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
 
-			#calculate the new position
-			x = self.x_values[-1] + x_step
-			y = self.y_values[-1] + y_step
-
-			#append the new position to the list
-			self.x_values.append(x)
-			self.y_values.append(y)
+            #append the new position to the list
+            self.x_values.append(x)
+            self.y_values.append(y)
+            self.locations.append((x,y))
--- random-practise/chapter15/rw_visual.py
+++ random-practise/chapter15/rw_visual.py
@@ -1,22 +1,40 @@
 import matplotlib.pyplot as plt
-
+import matplotlib.artist as art
 from random_walk import RandomWalk
 
 rw = RandomWalk()
 rw.fill_walk()
+counts = dict()
 
 plt.style.use("classic")
 fig, ax = plt.subplots()
-point_numbers = range(rw.num_points)
+point_numbers = list(range(rw.num_points))
 
+#art.Artist.set_label("dance", point_numbers)
 
-ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+#print(point_numbers)
+
+for (xvalue, yvalue) in rw.locations:
+    counts[(xvalue,yvalue)] = counts.get((xvalue,yvalue),0) + 1
+
+#print(counts)
+frequency_list = list(counts.values())
+print("length of counts is {} and frequency_list is {}".format(len(counts), len(frequency_list)))
+
+#print(frequency_list)
+
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15, label='path')
 
 #setting the start and end point differently
 ax.scatter(0, 0, c='green', edgecolors='none', s=100)
 ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
+#ax.get_legend().set_visible(True)
+#leg.set_bbox_to_anchor((1.15,0.5))
+ax.legend()
+ax.get_xaxis().set_visible(True)
+ax.get_yaxis().set_visible(True)
 
 plt.show()
+#print(rw.x_values, rw.y_values)
+#print(rw.locations)
